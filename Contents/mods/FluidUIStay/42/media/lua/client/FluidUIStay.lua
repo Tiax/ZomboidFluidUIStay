53a24a8dbdefa6daed00ed6597dbaff3fda3beaf
Tiax <git@tiaxx.de>
2025-03-24 20:33:57 +0100

    Updated for 42.6.0

--- Contents/mods/FluidUIStay/42/media/lua/client/FluidUIStay.lua
+++ Contents/mods/FluidUIStay/42/media/lua/client/FluidUIStay.lua
@@ -27,6 +27,7 @@ function FluidUIStay.swap(ui)
   ui:validatePanel();
 end
 
+-- Add an extra handler for our new "Max Transfer"
 function FluidUIStay.onButton(ui, btn)
   -- fluidUI=self.parent would probably also work
   ui.slider:setCurrentValue(1.0) -- set slider to max
@@ -54,8 +55,10 @@ function ISFluidTransferUI.OpenPanel(_player, _container, _source) -- void
 end
 
 -- Overwrite: ISFluidTransferUI:prerender
+-- we rewrite the original to draw on self.btnTransfer.parent instead of self,
+-- this fixes a rendering bug, where the progress bar is otherwise not properly rendered (over the button)
 function ISFluidTransferUI:prerender()
-    ISPanel.prerender(self);
+    ISPanelJoypad.prerender(self);
 
     --draws a background for transfer button and action progress if action exists.
     if self.btnTransfer then
@@ -81,7 +84,7 @@ function ISFluidTransferUI:createChildren()
   FluidUIStay.orgISFluidTransferUIcreateChildren(self);
 
   -- FIX: an empty container should go right side (target), otherwise left side (source)
-  -- this is currently (42.0.2) reversed for some reason (ISFluidTransferUI.lua:107):
+  -- this is currently (last checked 42.6.0) reversed for some reason (ISFluidTransferUI.lua:107):
   local panelLeft = self.panelLeft;
 
   if panelLeft then
@@ -99,7 +102,7 @@ function ISFluidTransferUI:createChildren()
     end
   end
 
-  -- FEATURE: replace the max label with a button
+  -- FEATURE: replace the "Max Transfer" label with a button
   if self.maxTransferLabel then
     local c = self.buttonBorderColor;
 
@@ -135,31 +138,11 @@ function ISFluidContainerPanel:clickedDropBox(x, y)
     return
   end
 
-  for i, opt in ipairs(context.options) do
+  for _, opt in ipairs(context.options) do
     local item = opt.param1
 
-    -- apparently ISContextMenu doesn't render including v:getTextureFluidMask(), pity!
     if item then
-      opt.iconTexture =  item:getTex()
+      opt.itemForTexture = item
     end
   end
-
-  -- the context's size likely changed because of the icons, let's reposition
-  local parent = self.parent -- ISFluidContainerPanel
-  local parentUI = parent.parent -- ISFluidInfoUI/ISFluidTransferUI
-  local xx = parentUI:getAbsoluteX() + parentUI:getWidth()
-  local yy = parentUI:getAbsoluteY() + parent:getY()
-
-  context:setWidth(context:calcWidth())
-  context:calcHeight()
-
-  if parent.isLeft then
-    xx = parentUI:getAbsoluteX() - context:getWidth()
-    context:setSlideGoalX(xx - 20, xx)
-  else
-    context:setSlideGoalX(xx + 20, xx)
-  end
-
-  context:setSlideGoalY(yy - 10, yy)
-  context:bringToTop()
 end
